acba3f5237a8b36ae0e4c1be5201a080f4f193a0
fahrymaodah <mfahry96@gmail.com>
2026-02-07 02:43:23 +0800

    Add new tutor button

--- app/Filament/Resources/TutorResource/Pages/ListTutors.php
+++ app/Filament/Resources/TutorResource/Pages/ListTutors.php
@@ -3,9 +3,17 @@
 namespace App\Filament\Resources\TutorResource\Pages;
 
 use App\Filament\Resources\TutorResource;
+use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
 
 class ListTutors extends ListRecords
 {
     protected static string $resource = TutorResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            CreateAction::make(),
+        ];
+    }
 }
